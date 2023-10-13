2fbe571524afdfc39a49640caac0db7660657cd4
Ameer-M <t3hdamnedone@gmail.com>
2023-10-13 16:59:32 +0300

    Setting up TCP connect scan

--- scanner.py
+++ scanner.py
@@ -33,3 +33,10 @@ class Scanner:
             sr1(self.rest_packet, verbose=0,timeout=1)
 
         return self.response
+    
+    def connect(self):
+        # Implement try, except with this: https://0xbharath.github.io/art-of-packet-crafting-with-scapy/network_recon/service_discovery/index.html
+        self.ip_layer = IP(dst=self.ip)
+        self.syn_packet = TCP(dport=self.port, flags="S", seq=100)
+        self.syn_response = sr1(self.ip_layer/self.syn_packet)
+
