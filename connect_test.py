ee263a6cd0a5ba33875d29f4504027c9fae3087f
Ameer-M <t3hdamnedone@gmail.com>
2023-10-14 17:50:33 +0300

    updated TCP connect test script

--- connect_test.py
+++ connect_test.py
@@ -3,8 +3,13 @@ from scapy.all import *
 
 target_ip = IP(dst="45.33.32.156")
 
-syn = TCP(dport=80, flags="S", seq=100)
+syn = TCP(dport=80, sport=4444, flags="S", seq=100)
 
 syn_packet = sr1(target_ip/syn)
 
-syn_packet.show()
+my_ack = syn_packet.seq + 1
+
+ack_packet= TCP(dport=80, sport=4444, flags="A", seq=101, ack=my_ack)
+final_packet = send(target_ip/ack_packet)
+
+final_packet.show()
